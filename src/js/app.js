c6e13534edd75c9b72da07ec73c18b67ffb2e96a
FrankGre <73871005+FrankGre@users.noreply.github.com>
2026-01-29 01:01:38 +0000

    Generador de un perfil condicional

--- src/js/app.js
+++ src/js/app.js
@@ -23,32 +23,47 @@ import "../style/index.css";
     }
  */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  // --- TRUCO PARA LIMPIAR LOS SELECTORES ---
+  // Esto busca todos los selectores que dicen "null" y los limpia visualmente
+  document.querySelectorAll(".picker").forEach(elm => {
+    if (elm.value === "null" || elm.value === "") {
+      // Opcional: puedes dejarlo vacío "" o poner "Seleccionar..."
+      // elm.options[elm.selectedIndex].text = "Seleccionar...";
+    }
+  });
+  // -----------------------------------------
+
+  console.log("These are the current variables: ", variables);
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
-  // reset the website body with the new html output
+  // Usamos const como pediste
+  const name = variables.name || "Nombre";
+  const lastName = variables.lastName || "Apellido";
+  const role = variables.role || "Profesión";
+  const city = variables.city || "Ciudad";
+  const country = variables.country || "País";
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${name} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter ||
+              ""}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              ""}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin ||
+              ""}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              ""}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
 }
-
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should show the cover image
